da101cb81232f84d6c64b3ba7e83e3ffc8d5146f
Jnmendza <jonatmendoza@gmail.com>
2020-09-22 02:07:06 -0700

    Message component built with props to render messages data

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,6 +3,8 @@ import './Chat.css';
 import { useParams } from 'react-router-dom';
 import db from "../firebase";
 
+import Message from './Message'
+
 // Material UI Icons
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
@@ -12,6 +14,7 @@ function Chat() {
     const { roomId } = useParams();
     // Stores the details in the rooms
     const [roomDetails, setRoomDetails] = useState(null)
+    const [roomMessages, setRoomMessages] = useState([])
 
     useEffect(() => {
         // only run this code if the roomId exist
@@ -22,10 +25,20 @@ function Chat() {
                 setRoomDetails(snapshot.data())
             ))
         }
+
+        // now get the collection of messages if the room has any msgs in asc order by time stamp
+        db.collection('rooms').doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot(snapshot => setRoomMessages(
+                snapshot.docs.map(doc => doc.data())
+            )
+        )
+
         // roomId is the dependency bc we need to update it every time it changes
     }, [roomId])
 
-
+    console.log("MESSAGES >>>", roomMessages)
     return (
         <div className="chat">
             <div className="chat__header">
@@ -42,6 +55,17 @@ function Chat() {
                         <InfoOutlinedIcon /> Details
                     </p>
                 </div>
+            </div>
+            {/* map thru every msg, deconstruct it and pass the props */}
+            <div className="chat__messages">
+                {roomMessages.map(({message, timestamp, user, userImage}) => (
+                    <Message 
+                    message={message}
+                    timestamp={timestamp}
+                    user={user}
+                    userImage={userImage}
+                    />
+                ))}
             </div>
         </div>
     )
